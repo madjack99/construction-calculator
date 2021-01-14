d806951aab3be8afc15eb57b1a12950ede57d06e
Alex <kavanokami@gmail.com>
2021-01-14 16:24:43 +0400

    feat: add redux dev tools

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 
 import { rootReducer } from './reducer';
 
-export const store = createStore(rootReducer);
+export const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(ReduxThunk),
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+      (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
